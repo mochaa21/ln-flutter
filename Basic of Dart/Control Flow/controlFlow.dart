41fee4613750e98aa644cfc1a30b133f234ae888
mochaa21 <mochamadsyahid573@gmail.com>
2026-05-05 19:44:35 +0700

    add if else in control flow

---
+++ Basic of Dart/Control Flow/controlFlow.dart
@@ -0,0 +1,17 @@
+void main() {
+  var yourGrade = 2059;
+
+  if (yourGrade >= 5000) {
+    print('Your grade is S');
+  } else if (yourGrade >= 4000) {
+    print('Your grade is A');
+  } else if (yourGrade >= 3000) {
+    print('Your grade is B');
+  } else if (yourGrade >= 2000) {
+    print('Your grade is C');
+  } else if (yourGrade >= 1000) {
+    print('Your grade is D');
+  } else if (yourGrade <= 500) {
+    print("you're dropout...");
+  }
+}
