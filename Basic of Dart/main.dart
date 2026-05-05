607fb5e09b1b9d60f98a86795f67ba97e56938a0
mochaa21 <mochamadsyahid573@gmail.com>
2026-05-05 19:06:12 +0700

    add const final

--- Basic of Dart/main.dart
+++ Basic of Dart/main.dart
@@ -1,7 +1,14 @@
-
 // case sensitive
 
 String namaVar = 'ini adalah string';
 int varAngka = 10;
 double varDouble = 65.20;
 bool varTrueOrFalse = true;
+
+// final vs const
+
+const varKonstan =
+    20; // harus diinisiasi kan sebelum code dijalankan (compile-time)
+final iniFinal = "ini bisa dilakukan saat aplikasi sedang berjalan";
+
+
