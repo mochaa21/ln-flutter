4442bc37cbad7496f6bcc3ab7ea3f4ad3d05d33d
mochaa21 <mochamadsyahid573@gmail.com>
2026-05-05 19:14:00 +0700

    null safety

--- Basic of Dart/main.dart
+++ Basic of Dart/main.dart
@@ -11,4 +11,9 @@ const varKonstan =
     20; // harus diinisiasi kan sebelum code dijalankan (compile-time)
 final iniFinal = "ini bisa dilakukan saat aplikasi sedang berjalan";
 
+//  null safety - variabel di dart tidak boleh null. tapi kalo diharuskan suatu variabel menjadi null, perlu diberi '?'
+String? iniVarNull; // kasih tanda tanya
+
+String check =
+    iniVarNull ?? "var ini null. ubah dibaris 15 jika pengen diisi value";
 
